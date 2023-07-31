1b3829ef4d41ed6de78b3e0d1e6810e54eb9cb94
pranavtayi <tayipranav@gmail.com>
2023-07-30 23:09:36 -0500

    API call works

--- app/src/main/java/com/example/myapplication/Scan.java
+++ app/src/main/java/com/example/myapplication/Scan.java
@@ -6,15 +6,12 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,21 +19,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Scan extends AppCompatActivity {
 
     private static final int REQUEST_CAMERA_PERMISSION = 100;
     private static final int REQUEST_IMAGE_CAPTURE = 200;
+    private static final String API_URL = "https://themoddedcube.pythonanywhere.com/classify"; // Replace with your API URL
 
     private Uri imageUri;
-    private byte[] imageInByte;
+    private TextView tvResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +49,18 @@ public class Scan extends AppCompatActivity {
         btnOpenCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(Scan.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(Scan.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                if (ContextCompat.checkSelfPermission(Scan.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Scan.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CAMERA_PERMISSION);
                 } else {
                     openCamera();
                 }
             }
         });
+
+        tvResponse = findViewById(R.id.tvResponse);
     }
 
     private void openCamera() {
@@ -61,102 +68,90 @@ public class Scan extends AppCompatActivity {
         values.put(MediaStore.Images.Media.TITLE, "New Picture");
         values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
         imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
     }
-
-    private void displayApiResponse(String responseMessage) {
-        // Since you want to update the UI from a background thread, you can use a Handler
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                // Update the UI with the API result (responseMessage)
-                Toast.makeText(Scan.this, "API Response: " + responseMessage, Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-
-    private void handleApiCall(String apiUrl, byte[] imageInByte) {
-        try {
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "image/jpeg");
-            connection.setDoOutput(true);
-            connection.getOutputStream().write(imageInByte);
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // API call successful, read the response
-                String responseMessage = readResponseMessage(connection);
-                displayApiResponse(responseMessage);
-            } else {
-                // API call failed
-                displayApiResponse("API call failed with response code: " + responseCode);
-            }
-
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-            displayApiResponse("Error during API call: " + e.getMessage());
-        }
-    }
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            // ... (same code as before)
-
-            try {
-                InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                Bitmap image = BitmapFactory.decodeStream(imageStream);
-                // ... (same code as before)
-
-                // Convert to byte array
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                imageInByte = baos.toByteArray(); // Assign the byte array to imageInByte
-
-                // Send to API using a Thread
-                String apiUrl = "http://themoddedcube.pythonanywhere.com/classify";
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        handleApiCall(apiUrl, imageInByte);
-                    }
-                }).start();
-
-            } catch (IOException e) {
-                Log.e("Image Upload", "Error uploading image", e);
+            if (imageUri != null) {
+                // Image captured and saved to the gallery
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    uploadImageToAPI(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Error reading image", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Error saving image", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    private void uploadImageToAPI(Bitmap bitmap) {
+        // Convert the bitmap to a byte array
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
 
+        // Create the OkHttpClient
+        OkHttpClient client = new OkHttpClient();
 
-    // Helper method to read the response message from the API
-    private String readResponseMessage(HttpURLConnection connection) throws IOException {
-        StringBuilder responseMessage = new StringBuilder();
-        InputStream inputStream = connection.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            responseMessage.append(line);
-        }
-        reader.close();
-        return responseMessage.toString();
+        // Create the multipart request body with the imageBytes
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", "image.jpg", RequestBody.create(MediaType.parse("image/jpeg"), imageBytes))
+                .build();
+
+        // Create the POST request
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .build();
+
+        // Make the API call asynchronously
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Handle the API call failure
+                        String errorMessage = "API call failed: " + e.getMessage();
+                        Toast.makeText(Scan.this, errorMessage, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // Get the API response as a String
+                final String apiResponse = response.body().string();
+
+                // Update the TextView with the API response
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvResponse.setText(apiResponse);
+                    }
+                });
+                // Close the response body after using it
+                response.close();
+            }
+        });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
         if (requestCode == REQUEST_CAMERA_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openCamera();
@@ -166,4 +161,3 @@ public class Scan extends AppCompatActivity {
         }
     }
 }
-
